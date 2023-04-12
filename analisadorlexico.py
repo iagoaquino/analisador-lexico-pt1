769c7c0a1b8d23cf92f7a0edbdf45a51a27546fb
iaquino <iagodeaquino10@gmail.com>
2023-04-12 19:07:07 -0300

    gerador de automatos criados + automato de exemplo

--- analisadorlexico.py
+++ analisadorlexico.py
@@ -1,69 +1,63 @@
-from tkinter import W
-
-
-entrada = "+5a3b2.5345a232a2"
-lexemas = []
-numeros = "0123456789"
-simbolo = "+-"
+from classes.automato import Automato
+sinais = "+-"
+digitos = "1234567890"
 ponto = "."
-def automato(estado_atual,alfabeto,cont_int,cont_float,lexema):
-    if estado_atual == 0:
-        for i in numeros:
-            if alfabeto == i:
-                lexema = lexema+str(alfabeto)
-                return 1, cont_int,cont_float,lexema
-        for i in simbolo:
-            if alfabeto == i:
-                lexema = lexema+str(alfabeto)
-                return 2, cont_int,cont_float,lexema
-    if estado_atual == 1:
-        for i in numeros:
-            if alfabeto == i:
-                lexema = lexema+str(alfabeto)
-                return 1, cont_int,cont_float,lexema
-        if alfabeto == ponto:
-            lexema = lexema+str(alfabeto)
-            return 4, cont_int,cont_float,lexema
-        else:
-            lexemas.append(lexema)
-            lexema = ""
-            estado_atual = 3
+letras = "qwertyuioasdfghjklpzxcvbnm"
+cont_int = 0
+cont_float = 0
+cadeia = "123ab2.3bac+1+2.32.0"
+#criação do automato que conta floats
+automato_int = Automato("+-1234567890qwertyuioasdfghjklpzxcvbnm.",4)
+for numero in digitos:
+    automato_int.adicionar_transicao(0,numero,1)
+    automato_int.adicionar_transicao(1,numero,1)
+    automato_int.adicionar_transicao(2,numero,1)
+for sinal in sinais:
+    automato_int.adicionar_transicao(0,sinal,2)
+    automato_int.adicionar_transicao_estrela(1,sinal,3)
+for letra in letras:
+    automato_int.adicionar_transicao_estrela(1,letra,3)
+    automato_int.adicionar_transicao(2,letra,0)
+automato_int.adicionar_transicao_estrela(1,".",3)
+automato_int.definir_aceitacao(1)
+estado_atual = 0
+for letra in cadeia:
+    estado_atual = automato_int.fazer_transicao(estado_atual,letra)
+    if automato_int.fazer_transicao_estrela(estado_atual,letra) == 1:
             cont_int+=1
-    if estado_atual == 2:
-        for i in numeros:
-            if alfabeto == i:
-                lexema = lexema+str(alfabeto)
-                return 1, cont_int,cont_float,lexema
-        return 0, cont_int,cont_float,lexema
-    if estado_atual == 4:
-        for i in numeros:
-            if alfabeto == i:
-                lexema = lexema+str(alfabeto)
-                return 4, cont_int,cont_float,lexema
-        lexemas.append(lexema)
-        lexema = ""
-        cont_float+=1
-        estado_atual = 3
-    if estado_atual == 3:
-        estado_atual = 0
-        estado_atual,cont_int,cont_float,lexema = automato(estado_atual,alfabeto,cont_int,cont_float,lexema)
-    return estado_atual,cont_int,cont_float,lexema
+            estado_atual = 0
+            print(letra)
+            automato_int.fazer_transicao(estado_atual,letra)
 
-
-def main():
-    cont_int = 0
-    cont_float = 0
-    estado_atual = 0
-    lexema = ""
-    for i in entrada:
-        alfabeto = i
-        estado_atual,cont_int,cont_float,lexema = automato(estado_atual,alfabeto,cont_int,cont_float,lexema)
-    if estado_atual == 1:
-        cont_int+=1
-        lexemas.append(lexema)
-    if estado_atual == 4:
-        cont_float+=1
-        lexemas.append(lexema)
-    print("inteiros contados: "+str(cont_int)+" , floats contados: "+str(cont_float))
-    print(lexemas)           
-main()
+if automato_int.finalizou_aceitou(estado_atual) == 1:
+    cont_int+=1
+#criando o automato que conta floats
+automato_float = Automato("+-1234567890qwertyuioasdfghjklpzxcvbnm.",6)
+automato_float.mostrar_alfabeto()
+for numero in digitos:
+    automato_float.adicionar_transicao(0,numero,1)
+    automato_float.adicionar_transicao(1,numero,1)
+    automato_float.adicionar_transicao(2,numero,1)
+    automato_float.adicionar_transicao(3,numero,4)
+    automato_float.adicionar_transicao(4,numero,4)
+for sinal in sinais:
+    automato_float.adicionar_transicao(0,sinal,2)
+    automato_float.adicionar_transicao_estrela(4,sinal,3)
+for letra in letras:
+    automato_float.adicionar_transicao_estrela(4,letra,5)
+    automato_float.adicionar_transicao(2,letra,0)
+    automato_float.adicionar_transicao(1,letra,0)
+    automato_float.adicionar_transicao(3,letra,0)
+automato_float.adicionar_transicao(1,".",3)
+automato_float.definir_aceitacao(4)
+estado_atual = 0
+for letra in cadeia:
+    estado_atual = automato_float.fazer_transicao(estado_atual,letra)
+    if automato_float.fazer_transicao_estrela(estado_atual,letra) == 1:
+            cont_float+=1
+            estado_atual = 0
+            print(letra)
+            automato_float.fazer_transicao(estado_atual,letra)
+if automato_float.finalizou_aceitou(estado_atual) == 1:
+    cont_float+=1
+print("quantidade de inteiros: "+str(cont_int)+",quantidade de floats:"+str(cont_float))
--- classes/automato.py
+++ classes/automato.py
@@ -1,10 +1,4 @@
-sinais = "+-"
-digitos = "1234567890"
-ponto = "."
-letras = "qwertyuioasdfghjklpzxcvbnm"
-cont_int = 0
-cadeia = "123ab23bac+1+23"
-#definição classe estado
+
 class Estado:
     def __init__(self):
         self.transicoes = []
@@ -33,7 +27,7 @@ class Automato:
             transicao.append(estado_transicao)
             self.estados[estado_atual].transicoes.append(transicao)
         else:
-            print("alfabeto não aceito")
+            print("alfabeto não aceito normal")
 
     def adicionar_transicao_estrela(self,estado_atual,alfabeto,estado_transicao):
         if alfabeto in self.alfabeto and estado_transicao in range(len(self.estados)):
@@ -42,7 +36,7 @@ class Automato:
             transicao.append(estado_transicao)
             self.estados[estado_atual].transicoes_estrela.append(transicao)
         else:
-            print("alfabeto não aceito") 
+            print("alfabeto não aceito estrela") 
 
     def definir_aceitacao(self,num_estado):
         self.estados_aceitacao.append(num_estado)
@@ -72,27 +66,3 @@ class Automato:
     def mostrar_tamanho_estados(self):
         print(len(self.estados))
 
-automato_int = Automato("+-1234567890qwertyuioasdfghjklpzxcvbnm.",4)
-for numero in digitos:
-    automato_int.adicionar_transicao(0,numero,1)
-    automato_int.adicionar_transicao(1,numero,1)
-    automato_int.adicionar_transicao(2,numero,1)
-for sinal in sinais:
-    automato_int.adicionar_transicao(0,sinal,2)
-    automato_int.adicionar_transicao_estrela(1,sinal,3)
-for letra in letras:
-    automato_int.adicionar_transicao_estrela(1,letra,3)
-    automato_int.adicionar_transicao(2,letra,0)
-automato_int.definir_aceitacao(1)
-estado_atual = 0
-for letra in cadeia:
-    estado_atual = automato_int.fazer_transicao(estado_atual,letra)
-    if automato_int.fazer_transicao_estrela(estado_atual,letra) == 1:
-            cont_int+=1
-            estado_atual = 0
-            print(letra)
-            automato_int.fazer_transicao(estado_atual,letra)
-
-if automato_int.finalizou_aceitou(estado_atual)== 1:
-    cont_int+=1
-print(cont_int)

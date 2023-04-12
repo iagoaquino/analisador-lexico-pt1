69e82760e9aebd229ead13588219ec5441364d52
iaquino <iagodeaquino10@gmail.com>
2023-04-12 17:54:10 -0300

    gerador de estados concluidos

--- classes/automato.py
+++ classes/automato.py
@@ -3,7 +3,7 @@ digitos = "1234567890"
 ponto = "."
 letras = "qwertyuioasdfghjklpzxcvbnm"
 cont_int = 0
-cadeia = "123ab23bac+1"
+cadeia = "123ab23bac+1+23"
 #definição classe estado
 class Estado:
     def __init__(self):
@@ -53,17 +53,16 @@ class Automato:
         return 0
 
     def fazer_transicao(self,estado_atual,alfabeto):
-        novo_estado = 0
+        novo_estado = estado_atual
         for i in range(len(self.estados[estado_atual].transicoes)):
             if alfabeto == self.estados[estado_atual].transicoes[i][0]:
-                print("entrei")
                 novo_estado = self.estados[estado_atual].transicoes[i][1]
-                print(novo_estado)
         return novo_estado
     
-    def fazer_transicao_estrela(self,estado_atual,alfabeto):
+    def fazer_transicao_estrela(self,estado_atual,letra):
         for i in range(len(self.estados[estado_atual].transicoes_estrela)):
-            for alfabeto in self.estados[estado_atual].transicoes_estrela[i]:
+            #print(letra+", transicoes estrela"+str(self.estados[estado_atual].transicoes_estrela[i][0]))
+            if letra == self.estados[estado_atual].transicoes_estrela[i][0]:
                 return 1
         return 0
             
@@ -80,14 +79,20 @@ for numero in digitos:
     automato_int.adicionar_transicao(2,numero,1)
 for sinal in sinais:
     automato_int.adicionar_transicao(0,sinal,2)
+    automato_int.adicionar_transicao_estrela(1,sinal,3)
 for letra in letras:
     automato_int.adicionar_transicao_estrela(1,letra,3)
     automato_int.adicionar_transicao(2,letra,0)
+automato_int.definir_aceitacao(1)
 estado_atual = 0
 for letra in cadeia:
     estado_atual = automato_int.fazer_transicao(estado_atual,letra)
     if automato_int.fazer_transicao_estrela(estado_atual,letra) == 1:
             cont_int+=1
             estado_atual = 0
+            print(letra)
             automato_int.fazer_transicao(estado_atual,letra)
+
+if automato_int.finalizou_aceitou(estado_atual)== 1:
+    cont_int+=1
 print(cont_int)
